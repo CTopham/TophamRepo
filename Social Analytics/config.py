32294accf29ad24473694f94332da904a51760f6
Craig <35710823+CTopham@users.noreply.github.com>
2018-03-30 16:48:10 -0400

    Adding Twitter analytics

---
+++ Social Analytics/config.py
@@ -0,0 +1,4 @@
+Consumer = "8wF9FY2WvJBv4oZAAyMe1cS04"
+ConsumerSecret = "E0FCEMDxe8UEYuTkUCwpwHEboioAXRNoEbhkh43m2tUOmHCxql"
+Access = "118578482-3H9CuZLWg5sWjh1UDuSoevxL80JUQRTu1KF3ueWW"
+AccessSecret = "Jatnl4jmlnRA3D7ufrSzDeI9FIkAji6E8GsfAIsQ9oBVU"
